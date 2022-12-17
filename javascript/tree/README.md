bd38927e8cce037ff0c4eec6b33bda0fed8e4c13
Seth <sethparkerpierce@icloud.com>
2022-12-16 20:19:17 -0500

    finished coding challenge 15 binary trees

---
+++ javascript/tree/README.md
@@ -0,0 +1,44 @@
+# Trees
+<!-- Short summary or background information -->
+Implementing the BinaryTree and BinarySearchTree classes.
+
+## Challenge
+<!-- Description of the challenge -->
+
+### Binary Tree
+
+- Create a Binary Tree class
+- Define a method for each of the depth first traversals:
+  - pre order
+  - in order
+  - post order
+Each depth first traversal method should return an array of values, ordered appropriately.
+
+### Binary Search Tree
+
+- Create a Binary Search Tree class
+  - This class should be a sub-class (or your languages equivalent) of the Binary Tree Class, with the following additional methods:
+    - Add
+      - Arguments: value
+      - Return: nothing
+      - Adds a new node with that value in the correct location in the binary search tree.
+    - Contains
+      - Argument: value
+      - Returns: boolean indicating whether or not the value is in the tree at least once.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+I used recursion in each of my methods, I believe the Big O for space for each is O(n) and time is O(log(n))
+
+## API
+<!-- Description of each method publicly available in each of your trees -->
+
+- BinaryTree -
+  - preOrder - starts from root, goes left, and then right, adds values into array, returns array
+  - inOrder - returns array of each value in tree from least to greatest
+  - postOrder - goes left first, then right , and then root value of tree
+
+- BinarySearchTree -
+  - add - adds value to tree in correct position
+  - contains - displays whether or not the value inputted is inside the tree returns true or false
---
+++ javascript/tree/__tests__/tree.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const {BinaryTree, Node, BinarySearchTree} = require('../index');
+
+describe('Tree', () => {
+  it('Can successfully instantiate an empty tree', () => {
+    let tree = new BinaryTree();
+    expect(tree).toBeInstanceOf(BinaryTree);
+  });
+  it('Can successfully instantiate a tree with a single root node', () => {
+    let tree = new BinaryTree();
+    tree.root = new Node(5);
+    expect(tree.root).toBeInstanceOf(Node);
+  });
+  it('For a Binary Search Tree, can successfully add a left child and right child properly to a node', () => {
+    let searchTree = new BinarySearchTree();
+    searchTree.add(10);
+    searchTree.add(5);
+    searchTree.add(15);
+    expect(searchTree.root.left.value).toBe(5);
+    expect(searchTree.root.right.value).toBe(15);
+  });
+  it('Can successfully return a collection from a preorder traversal', () => {
+    let searchTree = new BinarySearchTree();
+    searchTree.add(10);
+    searchTree.add(5);
+    searchTree.add(15);
+    expect(searchTree.preOrder()).toEqual([10,5,15]);
+  });
+  it('Can successfully return a collection from a inorder traversal', () => {
+    let tree = new BinaryTree();
+    tree.root = new Node(10);
+    tree.root.left = new Node(5);
+    tree.root.right = new Node(15);
+    tree.root.left.left = new Node(1);
+    tree.root.left.right = new Node(8);
+    tree.root.right.right = new Node(17);
+    expect(tree.inOrder()).toEqual([1,5,8,10,15,17]);
+  });
+  it('Can successfully return a collection from a postorder traversal', () => {
+    let tree = new BinaryTree();
+    tree.root = new Node(10);
+    tree.root.left = new Node(5);
+    tree.root.right = new Node(15);
+    tree.root.left.left = new Node(1);
+    tree.root.left.right = new Node(8);
+    tree.root.right.right = new Node(17);
+    expect(tree.postOrder()).toEqual([1,8,5,17,15,10]);
+  });
+  it('Returns true or false for the contains method, given an existing or non-existing node value', () => {
+    let searchTree = new BinarySearchTree();
+    searchTree.add(10);
+    searchTree.add(5);
+    searchTree.add(15);
+    expect(searchTree.contains(5)).toEqual(true);
+    expect(searchTree.contains(7)).toEqual(false);
+  });
+});
