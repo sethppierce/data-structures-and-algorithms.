92fa19cf4988377aa0788a34ede005dd73b648c6
Seth <sethparkerpierce@icloud.com>
2022-12-19 16:13:08 -0500

    finished code challenge 16

--- javascript/tree/index.js
+++ javascript/tree/index.js
@@ -59,6 +59,23 @@ class BinaryTree {
     traverse(this.root);
     return results;
   }
+
+  treeMax(){
+    let max = 0;
+    const traverse = (node) => {
+      if(node.value > max) max = node.value;
+      if(node.left){
+        traverse(node.left);
+
+      }
+      if(node.right){
+        traverse(node.right);
+      }
+    };
+    traverse(this.root);
+    return max;
+
+  }
 }
 
 class BinarySearchTree extends BinaryTree {
